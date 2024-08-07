4832bc3ade9fdc7f191915ec6f983aed2258b481
mahboob <shaikhm357@gmail.com>
2024-08-08 00:12:39 +0530

    create a configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy.user';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular_course_v18';
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
+  selectedUserId = 'u2';
 
-  onSelectUserr(id: string){
-    console.log('Selected user with id ' + id)
+  get selectedUser() {
+    return this.users.find((usr) => usr.id === this.selectedUserId)!;
   }
 
+  onSelectUserr(id: string) {
+    // console.log('Selected user with id ' + id)
+    this.selectedUserId = id;
+  }
 }
