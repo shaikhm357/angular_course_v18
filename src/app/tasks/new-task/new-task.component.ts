e3b22dc82722186636ce5c138043b012ca2a6c0d
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2025-01-22 22:23:40 +0530

    anorher way of injection

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, inject, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,24 +11,28 @@ import { NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>()
-  @Output() add = new EventEmitter<NewTaskData>()
+  @Input({ required: true }) userId!: string
+  @Output() close = new EventEmitter<void>()
+  // @Output() add = new EventEmitter<NewTaskData>()
 
   enteredTitle = ''
   enteredSummary = ''
   enteredDate = ''
 
+  private tasksService = inject(TasksService)
+
 
   onCancel() {
-    this.cancel.emit()
+    this.close.emit()
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    })
+    }, this.userId)
+    this.close.emit()
   }
 
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -33,14 +33,7 @@ export class TaskComponents {
     this.isAddingTask = true
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false
   }
-
-  onAddTasks(taskData: NewTaskData) {
-    this.tasksService.addTask(taskData, this.userId)
-    this.isAddingTask = false
-
-  }
-
 }
