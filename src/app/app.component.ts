46cb1a78edf4c1962667fe9e86d81408124bc166
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2025-01-22 15:31:23 +0530

    Revert "implemented old way was *ngIf"
    This reverts commit c6ff44eac5896db8007a9fba19d312785f4e80d1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy.user';
 import { TaskComponent } from './task/task.component';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
