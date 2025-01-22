c6ff44eac5896db8007a9fba19d312785f4e80d1
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2025-01-22 15:30:47 +0530

    implemented old way was *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy.user';
 import { TaskComponent } from './task/task.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
