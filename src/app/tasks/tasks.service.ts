c370d8495448d9aef3dc5e0d1e799d13cf95ee19
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2025-01-22 22:39:16 +0530

    implemented loacalstorage to save data

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,6 +6,13 @@ import { NewTaskData } from "./task/task.model";
 export class TasksService {
     public tasks = DUMMY_TASKS
 
+    constructor() {
+        const tasks = localStorage.getItem('tasks')
+        if (tasks) {
+            this.tasks = JSON.parse(tasks)
+        }
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId)
     }
@@ -18,10 +25,16 @@ export class TasksService {
             summary: taskData.summary,
             dueDate: taskData.date
         })
+        this.saveTasks()
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((tsk) => tsk.id !== id)
+        this.saveTasks()
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
 
 }
