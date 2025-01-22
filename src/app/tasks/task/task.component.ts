53872e0ad57c7c90ef17b393b93586921939aacc
Shaikh Mahboob <mahboob.shaikh@alwrite.com>
2025-01-22 20:15:09 +0530

    implemented ng-content feature for card

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
 // import { EventEmitter } from 'stream';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
